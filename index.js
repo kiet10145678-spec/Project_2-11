3bed19b6a541bfab9c1edde3144545b89ddb0765
kiet10145678-spec <kiet10145678@gmail.com>
2025-11-02 11:57:20 +0700

    add file js

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+console.log("Day la noi dung");
+
+let fullname = "Nguyen Van A";
+let age = 30;
+let isMarried = false;
+
+let firstName = "Nguyen";
+let lastName = "Van A";
+let email ="idk@gmail.com";
+let create = "01/01/2024";
+let birhtDay = "01/01/1994";
+let update = "10/06/2024";
+let deletedDate = null;
+
+console.log(firstName, lastName, email, create, birhtDay, update, deletedDate);
+
+let result = age + 10;
+console.log("Result after adding 10 to age:", result);
+
+let str = 'hello';
+console.log('So ky tu trong chuoi str la:', str.length);
